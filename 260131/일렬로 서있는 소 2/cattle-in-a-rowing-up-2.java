44b3b266bb7a1d35e4bdb547009f9fefe307e226
JuhanKim <151847638+rainshot00@users.noreply.github.com>
2026-01-31 21:31:18 +0900

    Add files via upload

---
+++ 260131/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int ans = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] <= arr[j] && arr[j] <= arr[k]) {
+                        ans++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
