0cf1207faadaca0b760fa7400e42dee6316f65ef
JuhanKim <151847638+rainshot00@users.noreply.github.com>
2026-02-19 15:11:20 +0900

    Title: 최대 직사각형 합, Time: 387ms, Memory: 18MB, Status: Passed - Codetree

--- 260219/최대 직사각형 합/max-rect-sum-in-grid.java
+++ 260219/최대 직사각형 합/max-rect-sum-in-grid.java
@@ -11,28 +11,27 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		int ans = -987654321;
 		int[][] arr = new int[n + 1][n + 1];
-		int[][] psum = new int[n + 1][n + 1];
+		int[][] rowPsum = new int[n + 1][n + 1];
 
 		for (int i = 1; i <= n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
 			for (int k = 1; k <= n; k++) {
 				arr[i][k] = Integer.parseInt(st.nextToken());
+				rowPsum[i][k] = rowPsum[i][k - 1] + arr[i][k];
 			}
 		}
 
-		for (int i = 1; i <= n; i++) {
-			for (int k = 1; k <= n; k++) {
-				psum[i][k] = psum[i - 1][k] + psum[i][k - 1] - psum[i - 1][k - 1] + arr[i][k];
-			}
-		}
+		for (int x1 = 1; x1 <= n; x1++) {
+			for (int x2 = x1; x2 <= n; x2++) {
+				int currentMax = 0;
 
-		for (int y1 = 1; y1 <= n; y1++) {
-			for (int x1 = 1; x1 <= n; x1++) {
-				for (int y2 = y1; y2 <= n; y2++) {
-					for (int x2 = x1; x2 <= n; x2++) {
-						ans = Math.max(ans, psum[y2][x2] - psum[y1 - 1][x2] - psum[y2][x1 - 1] + psum[y1 - 1][x1 - 1]);
-					}
+				for (int y = 1; y <= n; y++) {
+					int rowSum = rowPsum[y][x2] - rowPsum[y][x1 - 1];
+
+					// Kadane's Algorithm
+					currentMax = Math.max(rowSum, currentMax + rowSum);
+					ans = Math.max(ans, currentMax);
 				}
 			}
 		}
