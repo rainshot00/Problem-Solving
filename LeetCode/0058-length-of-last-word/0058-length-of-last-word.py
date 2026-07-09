5b58a835fb3f531ece1e92b7d587de3bff8e090c
rainshot00 <clfitdev@gmail.com>
2026-07-09 14:24:11 +0900

    Remove old synced LeetCode folder

--- LeetCode/0058-length-of-last-word/0058-length-of-last-word.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        arr = list(map(str, s.split()))
-        
-        return len(arr[-1])
--- LeetCode/1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minPartitions(self, n: str) -> int:
-        ans = -1
-
-        for digit in n:
-            ans = max(ans, int(digit))
-        
-        return ans
